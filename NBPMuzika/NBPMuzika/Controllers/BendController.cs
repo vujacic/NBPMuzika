1b9a8a7dd2e41a8b949fa55435fdce54318c1ab3
dmanic747 <darko.manic@elfak.rs>
2018-01-15 22:49:44 +0100

    Dodata strana za bendove.

---
+++ NBPMuzika/NBPMuzika/Controllers/BendController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NBPMuzika.Models.Baze;
+using NBPMuzika.Models.Entiteti;
+
+namespace NBPMuzika.Controllers
+{
+    public class BendController : Controller
+    {
+        Neo4j neo;
+        Redis redis;
+
+        public BendController()
+        {
+            neo = new Neo4j();
+            redis = new Redis();
+        }
+
+
+        // GET: Bend
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Bend/Details/5
+        public ActionResult Details(int id)
+        {
+            BendModel bm = new BendModel();
+            bm.Bendovi = neo.vratiBendPoID(id);
+            bm.Albumi = neo.sviAlbumiGrupe(id);
+            bm.Izvodjaci = neo.sviClanoviGrupe(id);
+
+            string zaKesiranje = bm.Bendovi[0].id + "." + bm.Bendovi[0].name + "/Bend";
+
+            
+            redis.IncrementTopPages(zaKesiranje);
+            redis.UpdateRecentPages(zaKesiranje);
+
+            return View(bm);
+        }
+
+        
+    }
+}
---
+++ NBPMuzika/NBPMuzika/Models/Entiteti/BendModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NBPMuzika.Models.Entiteti
+{
+    public class BendModel
+    {
+        public List<Bend> Bendovi { get; set; }
+        public List<Izvodjac> Izvodjaci { get; set; }
+        public List<Album> Albumi { get; set; }
+    }
+}
